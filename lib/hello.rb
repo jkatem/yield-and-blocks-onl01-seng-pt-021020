5eca63dd7677562900707bbb765a4b0045370598
Jen Miao <jenmiao@gmail.com>
2020-02-19 01:52:03 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,15 @@
-def yield_example
-	puts "Hi I'm having a nice day."
-	yield if block_given?
-	puts "How did you get here?!"
+def hello_t(array)
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
 end
 
-puts yield_example {puts "I AM A YIELD BLOCK INTERUPTING UR CODE"}
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
